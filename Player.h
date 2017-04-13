735bfc1a2d718ba2dc157ee12e7af3b408eae6e9
hackylr <to_avchen@hotmail.com>
2017-04-13 17:25:23 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,19 +1,26 @@
 #pragma once
 #include <SFML\Graphics.hpp>
 #include "Animation.h"
+#include "Collider.h"
 
 class Player
 {
 public:
-	Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed);
+	Player(sf::Texture* texture, sf::Vector2u imageCount, float switchTime, float speed, float jumpHeight);
 	~Player();
 
 	void Update(float deltaTime);
 	void Draw(sf::RenderWindow& window);
+	void onCollision(sf::Vector2f direction);
 
 	sf::Vector2f GetPosition()
 	{
 		return body.getPosition();
+	}
+
+	Collider GetCollider()
+	{
+		return Collider(body);
 	}
 
 private:
@@ -22,5 +29,8 @@ private:
 	unsigned int row;
 	float speed;
 	bool faceRight;
-};
 
+	sf::Vector2f velocity;
+	bool canJump;
+	float jumpHeight;
+};
